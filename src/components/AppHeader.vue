60f9b7de28566cd6fd20092fbcef10e34eaf4221
danilopiccinni <danilopiccinni@gmail.com>
2023-03-28 16:52:11 +0200

    feat: AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,35 @@
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<template>
+
+    <div class="header">
+        <img src="/img/yu-gi-oh.jpg" alt="">
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+
+    .header {
+
+
+
+        img {
+
+            width: 100%;
+            object-fit: cover;
+            object-position: top;
+   
+
+        }
+
+    }
+</style>
