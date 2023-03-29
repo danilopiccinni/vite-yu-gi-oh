a32966c1b5a6fcfffb6fc8627673ec556256d0a9
danilopiccinni <danilopiccinni@gmail.com>
2023-03-29 16:33:15 +0200

    eliminato righe inutili

--- src/components/AppButtonsNumbCard.vue
+++ src/components/AppButtonsNumbCard.vue
@@ -47,8 +47,6 @@ import {store} from "../store.js"
 
                 }
             }
-
-
         }
     }
                
@@ -80,17 +78,6 @@ import {store} from "../store.js"
 
     <AppNameSearch @eseguiRicerca="eseguiRicerca()"></AppNameSearch>
 
-    <!-- <div class="container-ricerca">
-
-        <span v-if="store.cards.length > 0"> trovati : {{ store.cards.length }} card </span>
-        <span>filtra per nome</span>
-        <input v-model="store.nomeSearch" type="text" placeholder="inserisci qui il nome" required>
-        <button @click="eseguiRicerca()" >cerca</button>
-
-    </div> -->
-
-
-
 </template>
 
 <style scoped lang="scss">
@@ -104,16 +91,5 @@ import {store} from "../store.js"
         padding-top: 50px;
     }
 
-    .container-ricerca {
-        display: flex;
-        justify-content: center;
-        gap: 15px;
-
-        padding-top: 25px;
-
-        input {
-            width: 250px;
-        }
-    }
 
 </style>
